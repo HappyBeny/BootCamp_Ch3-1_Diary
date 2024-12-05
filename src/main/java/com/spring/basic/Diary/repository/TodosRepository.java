b4ae93693ec80195ca34b249e4168094154d4407
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-06 02:21:08 +0900

    find메서드 반환타입 Optional에서 List로 바꿈(값이 1개가 아님)

--- src/main/java/com/spring/basic/Diary/repository/TodosRepository.java
+++ src/main/java/com/spring/basic/Diary/repository/TodosRepository.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 public interface TodosRepository {
 
     Schedule saveSchedule(Schedule schedule);
-    Optional<Schedule> findById(Long id);
-    Optional<Schedule> findByWriter(String writer);
-    Optional<Schedule> findByDate(LocalDateTime dateTime);
+    List<Schedule> findById(Long id);
+    List<Schedule> findByWriter(String writer);
+    List<Schedule> findByDate(LocalDateTime dateTime);
     List<Schedule> showAllSchedules();
 }
