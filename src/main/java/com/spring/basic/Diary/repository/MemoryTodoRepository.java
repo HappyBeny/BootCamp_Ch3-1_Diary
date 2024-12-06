9b36211954a58733c1641fda7267b93b6416ef34
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-06 14:47:50 +0900

    날짜탐색 메서드 타입 수정

--- src/main/java/com/spring/basic/Diary/repository/MemoryTodoRepository.java
+++ src/main/java/com/spring/basic/Diary/repository/MemoryTodoRepository.java
@@ -3,7 +3,7 @@ package com.spring.basic.Diary.repository;
 import com.spring.basic.Diary.domain.Schedule;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -38,14 +38,14 @@ public class MemoryTodoRepository implements TodosRepository{
     }
 
     @Override
-    public List<Schedule> findByCreatedDate(LocalDateTime dateTime) {
+    public List<Schedule> findByCreatedDate(LocalDate date) {
         return List.of();
     }
 
     @Override
-    public List<Schedule> findByUpdatedDate(LocalDateTime dateTime) {
+    public List<Schedule> findByUpdatedDate(LocalDate date) {
         return schedules.values().stream()
-                .filter(schedule -> schedule.getUpdatedTime().equals(dateTime))
+                .filter(schedule -> schedule.getUpdatedTime().equals(date))
                 .collect(Collectors.toList());
     }
 
