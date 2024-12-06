ced7dd136bd3f338d736533e4ee629d64ba8bbb1
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-06 14:57:20 +0900

    일정 조회용 메서드 생성

--- src/main/java/com/spring/basic/Diary/controller/TodoController.java
+++ src/main/java/com/spring/basic/Diary/controller/TodoController.java
@@ -1,10 +1,18 @@
 package com.spring.basic.Diary.controller;
 
 
+import com.spring.basic.Diary.domain.Schedule;
 import com.spring.basic.Diary.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/todo")
 @RestController
@@ -20,5 +28,19 @@ public class TodoController {
     }
 
     // 기
+    @PostMapping("/schedules/new")
+    public String create(ScheduleForm form) {
+        Schedule schedule = new Schedule(form.getTodo(), form.getWriter(), form.getPassword());
 
+        System.out.println("schedule: " + schedule.getTodo());
+
+        todoService.createSchedule(schedule);
+
+        return "redirect:/schedules";
+    }
+
+    @GetMapping("/schedules")
+    public List<Schedule> list(String writer, LocalDate date) {
+        return todoService.getSchedules(writer, date);
+    }
 }
