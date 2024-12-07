daef2f22941edc9cfad84effc7a97a9eb57b04a4
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-07 15:52:13 +0900

    순수jdbc 저장, id검색 메서드 완성

--- src/main/java/com/spring/basic/Diary/repository/JdbcTodoRepository.java
+++ src/main/java/com/spring/basic/Diary/repository/JdbcTodoRepository.java
@@ -1,20 +1,81 @@
 package com.spring.basic.Diary.repository;
 
 import com.spring.basic.Diary.dto.Schedule;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+
 public class JdbcTodoRepository implements TodosRepository{
+
+    private final DataSource dataSource;
+
+    public JdbcTodoRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public Schedule save(Schedule schedule) {
-        return null;
+        String sql = "insert into schedule (title, description, date) values (?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, schedule.getTodo());
+
+            pstmt.executeUpdate();
+            rs = pstmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                schedule.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return schedule;
+        } catch (Exception e){
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
     }
 
     @Override
     public Optional<Schedule> findById(Long id) {
-        return Optional.empty();
+        String sql = "select * from schedule where id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                Schedule schedule = new Schedule();
+                schedule.setId(rs.getLong("id"));
+                schedule.setTodo(rs.getString("todo"));
+                return Optional.of(schedule);
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
     }
 
     @Override
@@ -35,5 +96,33 @@ public class JdbcTodoRepository implements TodosRepository{
     @Override
     public List<Schedule> findAll() {
         return List.of();
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private static void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try{
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
