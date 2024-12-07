c0c0f058cfc5ee069d33c5e4880d8bee5b3c898e
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-07 15:50:15 +0900

    순수 jdbc용 DB, 생성자 추가

--- src/main/java/com/spring/basic/Diary/SpringConfig.java
+++ src/main/java/com/spring/basic/Diary/SpringConfig.java
@@ -1,17 +1,22 @@
 package com.spring.basic.Diary;
 
+import com.spring.basic.Diary.repository.JdbcTodoRepository;
 import com.spring.basic.Diary.repository.MemoryTodoRepository;
 import com.spring.basic.Diary.repository.TodosRepository;
 import com.spring.basic.Diary.service.TodoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
     private final TodosRepository todosRepository;
+    private final DataSource dataSource;
 
-    public SpringConfig(TodosRepository todosRepository) {
+    public SpringConfig(TodosRepository todosRepository, DataSource dataSource) {
         this.todosRepository = todosRepository;
+        this.dataSource = dataSource;
     }
 
     @Bean
@@ -21,7 +26,8 @@ public class SpringConfig {
 
     @Bean
     public TodosRepository todosRepository() {
-        return new MemoryTodoRepository();
+//        return new MemoryTodoRepository();
+        return new JdbcTodoRepository(dataSource);
     }
 
     TodosRepository todo = new MemoryTodoRepository();
