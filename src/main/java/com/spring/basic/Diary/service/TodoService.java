78ba3b57f1e45d71081b68a85c99a86bfc17bea7
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-09 15:40:38 +0900

    id탐색 메서드 추가

--- src/main/java/com/spring/basic/Diary/service/TodoService.java
+++ src/main/java/com/spring/basic/Diary/service/TodoService.java
@@ -1,23 +1,24 @@
 package com.spring.basic.Diary.service;
 
-import com.spring.basic.Diary.dto.Schedule;
-import com.spring.basic.Diary.repository.MemoryTodoRepository;
+import com.spring.basic.Diary.dto.ScheduleDto;
+import com.spring.basic.Diary.entity.ScheduleEntity;
 import com.spring.basic.Diary.repository.TodosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
     //속
+    @Autowired
     private final TodosRepository todosRepository;
 
 
     //생
-    @Autowired
     public TodoService(TodosRepository todosRepository) {
         this.todosRepository = todosRepository;
     }
@@ -29,7 +30,7 @@ public class TodoService {
      * @param schedule 전달 받을 schedule 객체
      * @return 일정 생성 후 일정 내용 반환
      */
-    public String createSchedule(Schedule schedule) {
+    public String createSchedule(ScheduleDto schedule) {
         todosRepository.save(schedule);
         return schedule.getTodo();
     }
@@ -40,7 +41,7 @@ public class TodoService {
      * @param date 수정일
      * @return 입력된 값 갯수에 따라 각각의 리스트 반환.
      */
-    public List<Schedule> getSchedules(String writer, LocalDate date) {
+    public List<ScheduleEntity> getSchedules(String writer, LocalDate date) {
         if (writer == null && date == null) {
             return todosRepository.findAll();
         } else if (writer == null) {
@@ -52,6 +53,15 @@ public class TodoService {
                     .filter(schedule -> schedule.getWriter().equals(writer))
                     .collect(Collectors.toList());
         }
+    }
+
+    /**
+     * id로 조회하기(단건 일정 조회)
+     * @param id 식별용 고유 id 입력
+     * @return 입력된 id에 해당하는 객체 반환
+     */
+    public Optional<ScheduleEntity> getScheduleWithId(Long id) {
+        return todosRepository.findById(id);
     }
 
 }
